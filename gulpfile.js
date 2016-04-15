611059d23515d3be5ffaa14fa2921f4a01b7b494
Karin Huber <karin@timecockpit.com>
2016-04-15 15:37:08 +0200

    Website

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,26 @@
-﻿
+﻿var gulp = require("gulp");
+var concat = require("gulp-concat");
+var del = require("del");
+
+gulp.task("clean", function () {
+    del.sync(["scripts/*.js"]);
+    del.sync(["styles/*.css"]);
+});
+
+var vendorScripts = ["node_modules/jquery/dist/jquery.js", "node_modules/bootstrap/dist/js/bootstrap.js"];
+var vendorStyles = ["node_modules/bootstrap/dist/css/bootstrap.css"];
+
+// Combine and minify all scripts from the bower_components folder
+gulp.task("dependencyScriptsAndStyles", [], function () {
+    gulp.src(vendorScripts)
+		.pipe(concat("vendor.js"))
+		.pipe(gulp.dest("scripts/"));
+
+    gulp.src(vendorStyles)
+		.pipe(concat("vendor.css"))
+		.pipe(gulp.dest("styles/"));
+
+});
+
+//Set a default tasks
+gulp.task("default", ["clean", "dependencyScriptsAndStyles"], function () { });
